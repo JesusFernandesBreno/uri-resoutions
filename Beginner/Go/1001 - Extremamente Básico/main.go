9fd6b6981645dbe760b6d33e4905c4735878dbd7
Breno Fernandes <fernandesbrenojesus@gmail.com>
2019-10-26 19:58:38 -0300

    2753 - Saida 7

---
+++ Beginner/Go/1001 - Extremamente Básico/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	var n1, n2 int
+	fmt.Scanf("%d\n%d", &n1, %n2)
+	fmt.Printf("X = %d\n", n1+n2)
+}
---
+++ Beginner/Go/2753 - Saída 7/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	l := 'a'
+	n := 97
+	for i := 0; i <= 25; i++ {
+		
+		fmt.Printf("%d e %c\n", n+i, l + rune(i))
+	}
+}
